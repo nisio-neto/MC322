3bb28e8865f0e273cd99938617bfaf3282bdc6f1
riacrdohorie <ricardohorie07@gmail.com>
2023-08-13 22:19:39 -0300

    adicionando novo Emprestimo

---
+++ lab1/src/bem/Emprestimo.java
@@ -0,0 +1,73 @@
+package bem;
+
+import java.time.LocalDate;
+
+public class Emprestimo {
+	// atributo da Classe
+
+	private String livro;
+	private String usuario;
+	private Data dataEmprestimo;
+	private Data dataVencimento;
+	private boolean devolvido;
+	private int diasDeVencimento;
+	
+	
+	public static class Data {
+	    private int dia;
+	    private int mes;
+	    private int ano;
+	    
+	    public Data(LocalDate data) {
+            this.dia = data.getDayOfMonth();
+            this.mes = data.getMonthValue();
+            this.ano = data.getYear();
+	    }
+	    
+	    public int getDia() {
+	        return dia;
+	    }
+	    
+	    public int getMes() {
+	        return mes;
+	    }
+	    
+	    public int getAno() {
+	        return ano;
+	    }
+	    
+	    public void setDia(int dia) {
+	        this.dia = dia;
+	    }
+	    
+	    public void setMes(int mes) {
+	        this.mes = mes;
+	    }
+	    
+	    public void setAno(int ano) {
+	        this.ano = ano;
+	    }
+	    
+	    public String toString() {
+	        return dia + "/" + mes + "/" + ano;
+	    }
+	    
+}
+	
+
+
+	// Construtor da Classe
+	public Emprestimo(String livro, String usuario){
+		this.livro = livro;
+		this.usuario = usuario;
+		this.dataEmprestimo = new Data(LocalDate.now());
+		int diasDeVencimento = 14; // 14 dias de data de vencimento
+		LocalDate dataVencimento = LocalDate.now().plusDays(diasDeVencimento);
+		this.dataVencimento = new Data(dataVencimento);
+		this.devolvido = false;
+	}
+		
+	void marcarComoDevolvido(){
+		this.devolvido = true;
+	}
+}
