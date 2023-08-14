95722313b1264fb73babb1e870bd52ab385b63d8
riacrdohorie <82128101+riacrdohorie@users.noreply.github.com>
2023-08-13 22:38:41 -0300

    Update Emprestimo.java

--- lab1/src/bem/Emprestimo.java
+++ lab1/src/bem/Emprestimo.java
@@ -1,101 +1,69 @@
 package bem;
 
-<<<<<<< HEAD
 import java.time.LocalDate;
-=======
->>>>>>> d41b891a8e1bca176c21306e8afa85dbd55b0f1b
 
 public class Emprestimo {
-	// atributo da Classe
+    // Atributos da Classe
+    private String livro;
+    private String usuario;
+    private Data dataEmprestimo; 
+    private Data dataVencimento;  
+    private boolean devolvido;
 
-	private String livro;
-	private String usuario;
-<<<<<<< HEAD
-	private Data dataEmprestimo;
-	private Data dataVencimento;
-	private boolean devolvido;
-	private int diasDeVencimento;
-	
-	
-	public static class Data {
-=======
-	private String dataEmprestimo;
-	private String dataVencimento;
-	private boolean devolvido;
-	private String Data;
-	
-	public class Data {
->>>>>>> d41b891a8e1bca176c21306e8afa85dbd55b0f1b
-	    private int dia;
-	    private int mes;
-	    private int ano;
-	    
-<<<<<<< HEAD
-	    public Data(LocalDate data) {
+    // Construtor da Classe
+    public Emprestimo(String livro, String usuario) {
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimo = new Data(LocalDate.now());
+        int diasDeVencimento = 14; // Por exemplo, 14 dias de vencimento
+        LocalDate dataVencimento = LocalDate.now().plusDays(diasDeVencimento); // Adicione os dias de vencimento
+        this.dataVencimento = new Data(dataVencimento);
+
+        this.devolvido = false;
+    }
+
+    void marcarComoDevolvido() {
+        this.devolvido = true;
+    }
+
+    // Classe interna estática Data
+    public static class Data {
+        private int dia;
+        private int mes;
+        private int ano;
+
+        public Data(LocalDate data) {
             this.dia = data.getDayOfMonth();
             this.mes = data.getMonthValue();
             this.ano = data.getYear();
-=======
-	    public Data(int dia, int mes, int ano) {
-	        this.dia = dia;
-	        this.mes = mes;
-	        this.ano = ano;
->>>>>>> d41b891a8e1bca176c21306e8afa85dbd55b0f1b
-	    }
-	    
-	    public int getDia() {
-	        return dia;
-	    }
-	    
-	    public int getMes() {
-	        return mes;
-	    }
-	    
-	    public int getAno() {
-	        return ano;
-	    }
-	    
-	    public void setDia(int dia) {
-	        this.dia = dia;
-	    }
-	    
-	    public void setMes(int mes) {
-	        this.mes = mes;
-	    }
-	    
-	    public void setAno(int ano) {
-	        this.ano = ano;
-	    }
-	    
-	    public String toString() {
-	        return dia + "/" + mes + "/" + ano;
-	    }
-	    
-}
-<<<<<<< HEAD
-	
+        }
 
-=======
->>>>>>> d41b891a8e1bca176c21306e8afa85dbd55b0f1b
+        public int getDia() {
+            return dia;
+        }
 
-	// Construtor da Classe
-	public Emprestimo(String livro, String usuario){
-		this.livro = livro;
-		this.usuario = usuario;
-<<<<<<< HEAD
-		this.dataEmprestimo = new Data(LocalDate.now());
-		int diasDeVencimento = 14; // 14 dias de data de vencimento
-		LocalDate dataVencimento = LocalDate.now().plusDays(diasDeVencimento);
-		this.dataVencimento = new Data(dataVencimento);
-=======
-		this.dataEmprestimo = new Data();
-		this.dataVencimento = new Data();
-		this.dataVencimento.definirData(this.dataVencimento.getData()+ n); // n dias de vencimento
->>>>>>> d41b891a8e1bca176c21306e8afa85dbd55b0f1b
-		this.devolvido = false;
-	}
-		
-	void marcarComoDevolvido(){
-		this.devolvido = true;
-	}
+        public int getMes() {
+            return mes;
+        }
+
+        public int getAno() {
+            return ano;
+        }
+
+        public void setDia(int dia) {
+            this.dia = dia;
+        }
+
+        public void setMes(int mes) {
+            this.mes = mes;
+        }
+
+        public void setAno(int ano) {
+            this.ano = ano;
+        }
+
+        public String toString() {
+            return dia + "/" + mes + "/" + ano;
+        }
+    }
 }
