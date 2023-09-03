df2547a06ab28bb29269d74ab0e9729e40b9ab00
nisio-neto <njsneto22@gmail.com>
2023-09-03 20:15:04 -0300

    [MODIFIED] classes multimidias para cumprir requisitos do lab

--- src/RH/funcionarios.java
+++ src/RH/funcionarios.java
@@ -1,68 +1,84 @@
 package RH;
 
-public class funcionarios extends membro{
-	// Atributos da classe
-	private String cargo;
-	private String acesso;
-	private boolean Multa; // Indica se o usuario recebeu multa
-	private int DiasMulta; // indica dias de multa do usuario
-	
-	// Construtor da Classe
-	public funcionarios(String nome, String telefone, String CPF, String endereço, String dataInscricao, String cargo, String acesso ,boolean Multa, int DiasMulta) {
-		super(nome,telefone,CPF,endereço,dataInscricao);
-		this.cargo =  cargo;
-		this.acesso = acesso;
-		this.Multa = Multa;
-		this.DiasMulta = DiasMulta;
-		
-	}
-	// Metodos da classe
-	public void recadastraFuncionario(String Nome, String Telefone, String CPF, String endereço, String dataInscricao, String cargo, String acesso) {
-		this.setNome(Nome);
-		this.setTelefone(Telefone);
-		this.setendereço(endereço);
-		this.setCPF(CPF);
-		this.setDataInscricao(dataInscricao);
-		this.setAcesso(acesso);
-		this.setCargo(cargo);
-	}
-	public void aplicaMulta(boolean Multa, int DiasMulta) {
-		this.setMulta(Multa);
-		this.setDiasMulta(DiasMulta);
-	}
-	public void retiraMulta() {
-		this.setMulta(false);
-		this.setDiasMulta(0);
-	}
-	public double calcularTotalMultas() {
-		double valorMultaPorDia = 0.75; // Valor da multa por dia de atraso
-		return getDiasMulta() * valorMultaPorDia;
-	}
-	
-	// Getters e Setters
-	public void setCargo(String cargo) {
-		this.cargo = cargo;
-	}
-	public String getCargo() {
-		return cargo;
-	}	
-	public void setAcesso(String acesso) {
-		this.acesso = acesso;
-	}
-	public String getAcesso() {
-		return acesso;
-	}
-	public void setMulta(boolean Multa) {
-		this.Multa = Multa;
-	}
-	public boolean getMulta() {
-		return Multa;
-	}
-	public void setDiasMulta(int DiasMulta) {
-		this.DiasMulta = DiasMulta;
-	}
-	public int getDiasMulta() {
-		return DiasMulta;
-	}
+public class funcionarios extends membro {
+    // Atributos da classe
+    private String cargo;
+    private boolean Multa; // Indica se o usuário recebeu multa
+    private int DiasMulta; // indica dias de multa do usuário
+    private NivelAcesso nivelAcesso;
 
+    // Construtor da Classe
+    public funcionarios(String nome, String telefone, String CPF, String endereço, String dataInscricao, String cargo, boolean Multa, int DiasMulta, NivelAcesso nivelAcesso) {
+        super(nome, telefone, CPF, endereço, dataInscricao);
+        this.cargo = cargo;
+        this.Multa = Multa;
+        this.DiasMulta = DiasMulta;
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    // Métodos da classe
+    public void recadastraFuncionario(String nome, String telefone, String CPF, String endereço, String dataInscricao, String cargo, NivelAcesso nivelAcesso) {
+        this.setNome(nome);
+        this.setTelefone(telefone);
+        this.setendereço(endereço);
+        this.setCPF(CPF);
+        this.setDataInscricao(dataInscricao);
+        this.setCargo(cargo);
+        this.setNivelAcesso(nivelAcesso); // Adicionando o nível de acesso
+    }
+
+    public void aplicaMulta(boolean Multa, int DiasMulta) {
+        this.setMulta(Multa);
+        this.setDiasMulta(DiasMulta);
+    }
+
+    public void retiraMulta() {
+        this.setMulta(false);
+        this.setDiasMulta(0);
+    }
+
+    public double calcularTotalMultas() {
+        double valorMultaPorDia = 0.75; // Valor da multa por dia de atraso
+        return getDiasMulta() * valorMultaPorDia;
+    }
+
+    // Getters e Setters
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setMulta(boolean Multa) {
+        this.Multa = Multa;
+    }
+
+    public boolean isMulta() {
+        return Multa;
+    }
+
+    public void setDiasMulta(int DiasMulta) {
+        this.DiasMulta = DiasMulta;
+    }
+
+    public int getDiasMulta() {
+        return DiasMulta;
+    }
+
+    public NivelAcesso getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(NivelAcesso nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    // Enum para o nível de acesso
+    public enum NivelAcesso {
+        ADMINISTRADOR,
+        ATENDENTE,
+        GERENTE
+    }
 }
--- src/bem/DVD.java
+++ src/bem/DVD.java
@@ -2,67 +2,140 @@ package bem;
 
 public class DVD {
 
-	private String nome;
-	private String diretor;
-	private String estudio;
-	private String genero;
-	private int ano;
-	private String edicao;
-	private String id;
-	
-	public DVD(String nome, String diretor, String estudio, String genero, int ano, String edicao) {
-		this.nome = nome;
-		this.diretor = diretor; 
-		this.estudio = estudio;
-		this.genero = genero;
-		this.ano = ano;
-		this.edicao = edicao;
-		GeradorID gerador = new GeradorID(); // gerar ID do emprestimo
-        this.id = gerador.gerarId();
-	}
-	
-	// Getters e Setters
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setEstudio(String estudio) {
-		this.estudio = estudio;
-	}
-	public String getEstudio() {
-		return estudio;
-	}
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-	public String getGenero() {
-		return genero;
-	}
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
-	public int getAno() {
-		return ano;
-	}	
-	public String getDiretor() {
-		return diretor;
-	}
-	public void setDiretor(String diretor) {
-		this.diretor = diretor;
-	}
-	public String getEdicao() {
-		return edicao;
-	}
-	public void setEdicao(String edicao) {
-		this.edicao = edicao;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
+    private String nome;
+    private String diretor;
+    private String estudio;
+    private String genero;
+    private int ano;
+    private String edicao;
+    private String capa;
+    private String sinopse;
+    private String id;
+    private String elenco;
+    private String duracao;
+    private String idioma;
+    private String conservacao;
 
+    public DVD(String nome, String diretor, String estudio, String genero, int ano, String edicao, String capa, String sinopse, String elenco, String duracao, String idioma, String conservacao) {
+        this.nome = nome;
+        this.diretor = diretor;
+        this.estudio = estudio;
+        this.genero = genero;
+        this.ano = ano;
+        this.edicao = edicao;
+        this.capa = capa;
+        this.sinopse = sinopse;
+        this.elenco = elenco;
+        this.duracao = duracao;
+        this.idioma = idioma;
+        this.conservacao = conservacao;
+        GeradorID gerador = new GeradorID(); // gerar ID do empréstimo
+        this.id = gerador.gerarId();
+    }
+
+    // Getters e Setters para os atributos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public String getEstudio() {
+        return estudio;
+    }
+
+    public void setEstudio(String estudio) {
+        this.estudio = estudio;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getCapa() {
+        return capa;
+    }
+
+    public void setCapa(String capa) {
+        this.capa = capa;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getElenco() {
+        return elenco;
+    }
+
+    public void setElenco(String elenco) {
+        this.elenco = elenco;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getConservacao() {
+        return conservacao;
+    }
+
+    public void setConservacao(String conservacao) {
+        this.conservacao = conservacao;
+    }
 }
