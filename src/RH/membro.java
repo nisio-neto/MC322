59cb14008e65ed110b41728dc95c33fe75f3345f
riacrdohorie <ricardohorie07@gmail.com>
2023-09-03 20:35:35 -0300

    atualizando a classe membro para poder ser utilizado em renovação

--- src/RH/membro.java
+++ src/RH/membro.java
@@ -1,4 +1,5 @@
 package RH;
+
 import bem.GeradorID;
 
 public class membro {
@@ -9,8 +10,9 @@ public class membro {
 	private String CPF; // CPF do membro
 	private String endereço; // endereço da residencia do membro
 	private String dataInscricao;
+	private Perfil perfil;
 	
-	public membro(String nome, String telefone, String CPF, String endereço, String dataInscricao) {
+	public membro(String nome, String telefone, String CPF, String endereço, String dataInscricao, Perfil perfil) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.endereço = endereço;
@@ -18,8 +20,17 @@ public class membro {
 		this.dataInscricao = dataInscricao;
 		GeradorID gerador = new GeradorID(); // gerar ID do emprestimo
         this.id = gerador.gerarId();
+        this.perfil = perfil; // Definindo o perfil durante a criação do membro
 	}
 
+	// Enumeração para representar os perfis de membro
+	public enum Perfil {
+	    ESTUDANTE_GRADUACAO,
+	    ESTUDANTE_POS,
+	    PROFESSOR,
+	    FUNCIONARIO
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -58,4 +69,7 @@ public class membro {
 	public String getDataInscricao() {
 		return dataInscricao;
 	}
+	public Perfil getPerfil() {
+        return perfil; // Método para acessar o perfil do membro
+    }
 }
