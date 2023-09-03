922e75fc1f2bf71be994901add2aeb0188c180ed
riacrdohorie <ricardohorie07@gmail.com>
2023-09-03 20:44:54 -0300

    atualizando a classe de reserva com base nos parametros pedidos

--- src/bem/Reserva.java
+++ src/bem/Reserva.java
@@ -1,66 +1,74 @@
 package bem;
 
 import bem.GeradorID;
-import bem.Data;
+import RH.membro;
 
 public class Reserva {
+    private String livro;
+    private membro Membro;
+    private String id;
+    private Data dataReserva;
+    private boolean reservado;
+    private boolean disponivelParaEmprestimo; // variável para verificar se a cópia está disponível para empréstimo
 
+    public Reserva(String livro, membro Membro) {
+        this.livro = livro;
+        this.Membro = Membro;
+        this.dataReserva = new Data();
+        this.reservado = false;
+        this.disponivelParaEmprestimo = false; //  a cópia não está disponível para empréstimo
+        GeradorID gerador = new GeradorID();
+        this.id = gerador.gerarId();
+        System.out.println(id);
+    }
 
-	//Atributo da classe
-	private String livro; // nome do livro
-	private String usuario; // nome do usuario
-	private String id; // id da reserva
-	private Data dataReserva; // data da reserva
-	private boolean reservado; // indica se a reserva foi confirmada
+    // Método para confirmar a reserva
+    public void confirmaReserva() {
+        if (!reservado) {
+            if (disponivelParaEmprestimo) {
+                reservado = true;
+                System.out.println("Reserva confirmada para " + Membro.getNome() + " do livro " + livro);
+                disponivelParaEmprestimo = false; // Marca a cópia como não disponível para empréstimo após a confirmação
+            } else {
+                System.out.println("A cópia não está disponível para reserva.");
+            }
+        } else {
+            System.out.println("Esta reserva já foi confirmada anteriormente.");
+        }
+    }
 
-	//Construtor de classe
-	public Reserva(String livro, String usuario) {
-		this.livro = livro;
-		this.usuario = usuario;
-		this.dataReserva = new Data(); // data atual
-		GeradorID gerador = new GeradorID(); // gerar ID da reserva
-		this.id = gerador.gerarId();
-		this.reservado = false; // inicialmente não confirmada
-		}
+    // Método para reservar o livro
+    public void reservar() {
+        if (!reservado) {
+            if (!disponivelParaEmprestimo) {
+                disponivelParaEmprestimo = true; // Marca a cópia como disponível para empréstimo quando a reserva é feita
+                System.out.println("Reserva realizada para " + Membro.getNome() + " do livro " + livro);
+            } else {
+                System.out.println("Esta cópia já foi reservada anteriormente.");
+            }
+        } else {
+            System.out.println("Esta reserva já foi confirmada anteriormente.");
+        }
+    }
 
-	// confirmar a reserva
-	public void confirmaReserva() {
-		this.reservado = true;
-	}
-	
-	// Reservar o livro
-	public void reservar() {
-		if(!reservado) {
-			System.out.println("Reserva realizada para " + usuario + " do livro " + livro);
-			reservado = true;
-		} else {
-			System.out.println("Eta reserva já foi confirmada anteriormente.");
-		}
-	}
-	// Getter para ID
-	public String getId() {
-		return id;
-	}
-	
-	// Getter para livro
-	public String getLivro() {
-		return livro;
-	}
-	
-	// Getter para usuario
-	public String getUsuario() {
-		return usuario;
-	}
-	
-	// Getter para a data da reserva
-	public Data getDataReserva() {
-		return dataReserva;
-	}
-	
-	// Getter para confirmação da reserva
-	public boolean foiReservado() {
-		return reservado;
-	}
+    public String getId() {
+        return id;
+    }
+
+    public String getLivro() {
+        return livro;
+    }
+
+    public membro getMembro() {
+        return Membro;
+    }
+
+    public Data getDataReserva() {
+        return dataReserva;
+    }
+
+    public boolean foiReservado() {
+        return reservado;
+    }
 }
-
 
