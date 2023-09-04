9921f61cfb2246669af0750f5513a0d8050dae7e
nisio-neto <njsneto22@gmail.com>
2023-09-03 23:39:19 -0300

    [MODIFIED] Multimidia instanciada como classe pai-filhos

--- src/bem/CD.java
+++ src/bem/CD.java
@@ -1,98 +1,19 @@
 package bem;
 
-public class CD {
-    private String nome;
-    private String artista;
-    private String gravadora;
-    private String genero;
-    private int ano;
-    private String sinopse;
-    private String capa;
-    private String id;
+public class CD extends Item {
+
     private String faixas;
     private String duracao;
     private String conservacao;
 
     public CD(String nome, String artista, String gravadora, String genero, int ano, String sinopse, String capa, String faixas, String duracao, String conservacao) {
-        this.nome = nome;
-        this.artista = artista;
-        this.gravadora = gravadora;
-        this.genero = genero;
-        this.ano = ano;
-        this.sinopse = sinopse;
-        this.capa = capa;
+        super(nome, artista, gravadora, genero, ano, capa, sinopse);
         this.faixas = faixas;
         this.duracao = duracao;
         this.conservacao = conservacao;
-        GeradorID gerador = new GeradorID(); // gerar ID do emprestimo
-        this.id = gerador.gerarId();
     }
 
-    // Getters e Setters para os atributos
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setGravadora(String gravadora) {
-        this.gravadora = gravadora;
-    }
-
-    public String getGravadora() {
-        return gravadora;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public String getArtista() {
-        return artista;
-    }
-
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
-
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCapa() {
-        return capa;
-    }
-
-    public void setCapa(String capa) {
-        this.capa = capa;
-    }
-
+    // Getters e Setters para os atributos específicos de CD
     public String getFaixas() {
         return faixas;
     }
--- src/bem/Outros.java
+++ src/bem/Outros.java
@@ -1,128 +1,17 @@
 package bem;
 
-public class Outros {
+public class Outros extends Item {
 
-    private String nome;
-    private String autor;
-    private String editora;
-    private String genero;
-    private int ano;
-    private String edicao;
-    private String capa;
-    private String sinopse;
-    private String id;
-    private String local;
-    private String conservacao;
     private String tipo;
     private String formato;
 
     public Outros(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String local, String conservacao, String tipo, String formato) {
-        this.nome = nome;
-        this.autor = autor; 
-        this.editora = editora;
-        this.genero = genero;
-        this.ano = ano;
-        this.edicao = edicao;
-        this.capa = capa;
-        this.sinopse = sinopse;
-        this.local = local;
-        this.conservacao = conservacao;
+        super(nome, autor, editora, genero, ano, capa, sinopse);
         this.tipo = tipo;
         this.formato = formato;
-        GeradorID gerador = new GeradorID(); // gerar ID do empréstimo
-        this.id = gerador.gerarId();
     }
 
-    // Getters e Setters para os atributos
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public String getEditora() {
-        return editora;
-    }
-
-    public void setEditora(String editora) {
-        this.editora = editora;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
-    public String getEdicao() {
-        return edicao;
-    }
-
-    public void setEdicao(String edicao) {
-        this.edicao = edicao;
-    }
-
-    public String getCapa() {
-        return capa;
-    }
-
-    public void setCapa(String capa) {
-        this.capa = capa;
-    }
-
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getLocal() {
-        return local;
-    }
-
-    public void setLocal(String local) {
-        this.local = local;
-    }
-
-    public String getConservacao() {
-        return conservacao;
-    }
-
-    public void setConservacao(String conservacao) {
-        this.conservacao = conservacao;
-    }
-
+    // Getters e Setters para os atributos específicos de Outros
     public String getTipo() {
         return tipo;
     }
@@ -139,4 +28,3 @@ public class Outros {
         this.formato = formato;
     }
 }
-
