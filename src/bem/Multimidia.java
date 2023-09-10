d3b2be4d784a7a960b969e41511857c76b01d5b7
nisio-neto <njsneto22@gmail.com>
2023-09-10 17:03:53 -0300

    [MODIFIED] corrigidos erros na classe multimidia

--- src/bem/Multimidia.java
+++ src/bem/Multimidia.java
@@ -1,20 +1,21 @@
 package bem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Multimidia {
 
-    private DVD[] dvds;
-    private Livro[] livros;
-    private Ebook[] ebooks;
-    private CD[] cds;
-    private Outros[] outros;
+	private List<DVD> dvds;
+    private List<Livro> livros;
+    private List<Ebook> ebooks;
+    private List<CD> cds;
+    private List<Outros> outros;
     private int[] numItem;
     private boolean[][] disponivel;
     private int[][] unidades;
     private int[][] total;
     private int maxItens;
-    private int[] numItens;
-    private String[][] titulos;
-    private String[][] ids;
     private int[][] contadorEmprestimos;
     private int[][] contadorReservas;
     private int indice;
@@ -22,66 +23,63 @@ public class Multimidia {
     
 
     public Multimidia(int max_itens, int numTipos) {
-        dvds = new DVD[max_itens];
-        livros = new Livro[max_itens];
-        ebooks = new Ebook[max_itens];
-        cds = new CD[max_itens];
-        outros = new Outros[max_itens];
+    	dvds = new ArrayList<>();
+        livros = new ArrayList<>();
+        ebooks = new ArrayList<>();
+        cds = new ArrayList<>();
+        outros = new ArrayList<>();
         disponivel = new boolean[5][max_itens];  // 5 tipos de mídia (DVD, Livro, Ebook, CD, Outros)
         unidades = new int[5][max_itens];
         numItem = new int[5];
         total = new int[5][max_itens];
-        this.maxItens = maxItens;
-        numItens = new int[numTipos];
-        titulos = new String[numTipos][maxItens];
-        ids = new String[numTipos][maxItens];
+        this.maxItens = max_itens;
         contadorEmprestimos = new int[numTipos][maxItens];
         contadorReservas = new int[5][max_itens];
         
     }
 
-    public void adicionaDVD(String nome, String diretor, String estudio, String genero, int ano, String edicao, String capa, String sinopse, String elenco, String duracao, String idioma, String conservacao) {
+    public void adicionaDVD(String nome, String diretor, String estudio, String genero, int ano, String edicao, String capa, String sinopse, String elenco, String duracao, String idioma, String conservacao, int unidade) {
         DVD dvd = new DVD(nome, diretor, estudio, genero, ano, edicao, capa, sinopse, elenco, duracao, idioma, conservacao);
-        dvds[numItem[0]] = dvd;
-        unidades[0][numItem[0]] = unidades[0][numItem[0]];
-        disponivel[0][numItem[0]] = disponivel[0][numItem[0]];
-        total[0][numItem[0]] = numItem[0] + 1; // Atualiza o total de DVDs
+        dvds.add(dvd);
+        unidades[0][numItem[0]] = unidade;
+        disponivel[0][numItem[0]] = true;
+        total[0][numItem[0]] = numItem[0] + 1;
         numItem[0]++;
     }
 
-    public void adicionaLivro(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String isbn, String local, String conservacao) {
-        Livro livro = new Livro(nome, autor, editora, genero, ano, edicao, capa, sinopse, isbn, local, conservacao);
-        livros[numItem[1]] = livro;
-        unidades[1][numItem[1]] = unidades[1][numItem[1]];
-        disponivel[1][numItem[1]] = disponivel[1][numItem[1]];
-        total[1][numItem[1]] = numItem[1] + 1; // Atualiza o total de Livros
-        numItem[1]++;
-    }
-
-    public void adicionaEbook(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String formato, String formatoArquivo, String url, String requisitos, String dataDisponibilidade) {
+    public void adicionaEbook(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String formato, String formatoArquivo, String url, String requisitos, String dataDisponibilidade, int unidade) {
         Ebook ebook = new Ebook(nome, autor, editora, genero, ano, edicao, capa, sinopse, formato, formatoArquivo, url, requisitos, dataDisponibilidade);
-        ebooks[numItem[2]] = ebook;
-        unidades[2][numItem[2]] = unidades[2][numItem[2]];
-        disponivel[2][numItem[2]] = disponivel[2][numItem[2]];
-        total[2][numItem[2]] = numItem[2] + 1; // Atualiza o total de Ebooks
+        ebooks.add(ebook);
+        unidades[2][numItem[2]] = unidade;
+        disponivel[2][numItem[2]] = true;
+        total[2][numItem[2]] = numItem[2] + 1;
         numItem[2]++;
     }
 
-    public void adicionaCD(String nome, String artista, String gravadora, String genero, int ano, String sinopse, String capa, String faixas, String duracao, String conservacao) {
+    public void adicionaCD(String nome, String artista, String gravadora, String genero, int ano, String sinopse, String capa, String faixas, String duracao, String conservacao, int unidade) {
         CD cd = new CD(nome, artista, gravadora, genero, ano, sinopse, capa, faixas, duracao, conservacao);
-        cds[numItem[3]] = cd;
-        unidades[3][numItem[3]] = unidades[3][numItem[3]];
-        disponivel[3][numItem[3]] = disponivel[3][numItem[3]];
-        total[3][numItem[3]] = numItem[3] + 1; // Atualiza o total de CDs
+        cds.add(cd);
+        unidades[3][numItem[3]] = unidade;
+        disponivel[3][numItem[3]] = true;
+        total[3][numItem[3]] = numItem[3] + 1;
         numItem[3]++;
     }
 
-    public void adicionaOutros(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String local, String conservacao, String tipo, String formato) {
+    public void adicionaLivro(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String isbn, String local, String conservacao, int unidade) {
+        Livro livro = new Livro(nome, autor, editora, genero, ano, edicao, capa, sinopse, isbn, local, conservacao);
+        livros.add(livro);
+        unidades[1][numItem[1]] = unidade;
+        disponivel[1][numItem[1]] = true;
+        total[1][numItem[1]] = numItem[1] + 1;
+        numItem[1]++;
+    }
+
+    public void adicionaOutros(String nome, String autor, String editora, String genero, int ano, String edicao, String capa, String sinopse, String local, String conservacao, String tipo, String formato, int unidade) {
         Outros outro = new Outros(nome, autor, editora, genero, ano, edicao, capa, sinopse, local, conservacao, tipo, formato);
-        outros[numItem[4]] = outro;
-        unidades[4][numItem[4]] = unidades[4][numItem[4]];
-        disponivel[4][numItem[4]] = disponivel[4][numItem[4]];
-        total[4][numItem[4]] = numItem[4] + 1; // Atualiza o total de Outros
+        outros.add(outro);
+        unidades[4][numItem[4]] = unidade;
+        disponivel[4][numItem[4]] = true;
+        total[4][numItem[4]] = numItem[4] + 1;
         numItem[4]++;
     }
 
@@ -89,39 +87,51 @@ public class Multimidia {
 
     // Métodos para remover itens
     public void removeDVD(int indice) {
-        dvds[indice] = null;
-        disponivel[0][indice] = false;
-        unidades[0][indice] = 0;
-        numItem[0]--;
+        if (indice >= 0 && indice < dvds.size()) {
+            dvds.remove(indice);
+            disponivel[0][indice] = false;
+            unidades[0][indice] = 0;
+            numItem[0]--;
+        }
     }
 
     public void removeLivro(int indice) {
-        livros[indice] = null;
-        disponivel[1][indice] = false;
-        unidades[1][indice] = 0;
-        numItem[1]--;
+        if (indice >= 0 && indice < livros.size()) {
+            livros.remove(indice);
+            disponivel[1][indice] = false;
+            unidades[1][indice] = 0;
+            numItem[1]--;
+        }
     }
 
     public void removeEbook(int indice) {
-        ebooks[indice] = null;
-        disponivel[2][indice] = false;
-        unidades[2][indice] = 0;
-        numItem[2]--;
+        if (indice >= 0 && indice < ebooks.size()) {
+            ebooks.remove(indice);
+            disponivel[2][indice] = false;
+            unidades[2][indice] = 0;
+            numItem[2]--;
+        }
     }
 
     public void removeCD(int indice) {
-        cds[indice] = null;
-        disponivel[3][indice] = false;
-        unidades[3][indice] = 0;
-        numItem[3]--;
+        if (indice >= 0 && indice < cds.size()) {
+            cds.remove(indice);
+            disponivel[3][indice] = false;
+            unidades[3][indice] = 0;
+            numItem[3]--;
+        }
     }
 
     public void removeOutros(int indice) {
-        outros[indice] = null;
-        disponivel[4][indice] = false;
-        unidades[4][indice] = 0;
-        numItem[4]--;
+        if (indice >= 0 && indice < outros.size()) {
+            outros.remove(indice);
+            disponivel[4][indice] = false;
+            unidades[4][indice] = 0;
+            numItem[4]--;
+        }
     }
+    
+    
 
     public enum TipoMultimidia {
         DVD,
@@ -133,15 +143,16 @@ public class Multimidia {
     
  // Método para obter o número atual de itens multimídia de um tipo específico
     public int getNumItens(int tipo) {
-        if (tipo >= 0 && tipo < numItens.length) {
-            return numItens[tipo];
+        if (tipo >= 0 && tipo < numItem.length) {
+            return numItem[tipo];
         }
         return 0; // Retorna 0 se o tipo for inválido
     }
 
+
     // Método para obter o nome do tipo de um item multimídia
     public String getNomeTipo(int tipo) {
-        if (tipo >= 0 && tipo < numItens.length) {
+        if (tipo >= 0 && tipo < numItem.length) {
             switch (tipo) {
                 case 0:
                     return "DVD";
@@ -162,23 +173,44 @@ public class Multimidia {
 
     // Método para obter o ID de um item multimídia com base no tipo e no índice
     public String getId(int tipo, int indice) {
-        if (tipo >= 0 && tipo < numItens.length && indice >= 0 && indice < maxItens) {
-            return ids[tipo][indice];
+        if (tipo >= 0 && tipo < numItem.length && indice >= 0 && indice < maxItens) {
+            if (tipo == 0 && indice < dvds.size()) {
+                return dvds.get(indice).getId();
+            } else if (tipo == 1 && indice < livros.size()) {
+                return livros.get(indice).getId();
+            } else if (tipo == 2 && indice < ebooks.size()) {
+                return ebooks.get(indice).getId();
+            } else if (tipo == 3 && indice < cds.size()) {
+                return cds.get(indice).getId();
+            } else if (tipo == 4 && indice < outros.size()) {
+                return outros.get(indice).getId();
+            }
         }
         return null; // Retorna null se o tipo ou o índice forem inválidos
     }
 
     // Método para obter o título de um item multimídia com base no tipo e no índice
     public String getTitulo(int tipo, int indice) {
-        if (tipo >= 0 && tipo < numItens.length && indice >= 0 && indice < maxItens) {
-            return titulos[tipo][indice];
+        if (tipo >= 0 && tipo < numItem.length && indice >= 0 && indice < maxItens) {
+            if (tipo == 0 && indice < dvds.size()) {
+                return dvds.get(indice).getNome();
+            } else if (tipo == 1 && indice < livros.size()) {
+                return livros.get(indice).getNome();
+            } else if (tipo == 2 && indice < ebooks.size()) {
+                return ebooks.get(indice).getNome();
+            } else if (tipo == 3 && indice < cds.size()) {
+                return cds.get(indice).getNome();
+            } else if (tipo == 4 && indice < outros.size()) {
+                return outros.get(indice).getNome();
+            }
         }
         return null; // Retorna null se o tipo ou o índice forem inválidos
     }
+
   
     public int getNumeroTotalItens() {
         int total = 0;
-        for (int tipo = 0; tipo < numItens.length; tipo++) {
+        for (int tipo = 0; tipo < numItem.length; tipo++) {
             total += getNumItens(tipo);
         }
         return total;
@@ -186,10 +218,9 @@ public class Multimidia {
     
     //quantidade disponível de cada tipo de item multimídia
     public int getQuantidadeDisponivel(int tipo) {
-        if (tipo >= 0 && tipo < numItens.length) {
-            int totalItens = numItens[tipo];
+        if (tipo >= 0 && tipo < numItem.length) {
+            int totalItens = numItem[tipo];
             int[] emprestimosPorTipo = contadorEmprestimos[tipo];
-            
             int emprestados = 0;
             for (int emprestimos : emprestimosPorTipo) {
                 emprestados += emprestimos;
@@ -202,15 +233,15 @@ public class Multimidia {
 
  // Método para obter a quantidade de tipos de mídia
     public int getQuantidadeTipos() {
-        return numItens.length;
+        return numItem.length;
     }
     
    
         public int getTotalEmprestimos() {
             int totalEmprestimos = 0;
 
-            for (int tipo = 0; tipo < numItens.length; tipo++) {
-                for (int indice = 0; indice < numItens[tipo]; indice++) {
+            for (int tipo = 0; tipo < numItem.length; tipo++) {
+                for (int indice = 0; indice < numItem[tipo]; indice++) {
                     totalEmprestimos += contadorEmprestimos[tipo][indice];
                 }
             }
@@ -221,8 +252,8 @@ public class Multimidia {
         public int getReservas() {
             int totalReservas = 0;
 
-            for (int tipo = 0; tipo < numItens.length; tipo++) {
-                for (int indice = 0; indice < numItens[tipo]; indice++) {
+            for (int tipo = 0; tipo < numItem.length; tipo++) {
+                for (int indice = 0; indice < numItem[tipo]; indice++) {
                     totalReservas += contadorReservas[tipo][indice];
                 }
             }
@@ -231,25 +262,27 @@ public class Multimidia {
         }
 
     // Outros getters e setters
-    public DVD[] getDVDs() {
+
+    public List<DVD> getDVDs() {
         return dvds;
     }
 
-    public CD[] getCDs() {
-        return cds;
-    }
-
-    public Ebook[] getEbooks() {
-        return ebooks;
-    }
-
-    public Livro[] getLivros() {
+    public List<Livro> getLivros() {
         return livros;
     }
 
-    public Outros[] getOutros() {
-        return outros;
+    public List<Ebook> getEbooks() {
+    	return ebooks;
     }
+
+    public List<CD> getCDs() {
+    	return cds;
+    }
+
+    public List<Outros> getOutros() {
+    	return outros;
+    }
+
 
     public boolean[][] getDisponibilidade() {
         return disponivel;
