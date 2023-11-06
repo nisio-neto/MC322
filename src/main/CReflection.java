041149563a6addc2ad488ddb1bf77addb31483af
nisio-neto <njsneto22@gmail.com>
2023-11-06 13:36:09 -0300

    [MODIFIED] Classe completa para cobrir impressao de membros e midias

--- src/main/CReflection.java
+++ src/main/CReflection.java
@@ -1,36 +1,83 @@
 package main;
 import java.lang.reflect.*;
 import RH.membro;
+import bem.Multimidia;
 
 public class CReflection {
 
     public void ImprimeAtributoMembro() {
-        Class<?> ClasseMembro = membro.class;
+        try {
+            Class<?> ClasseMembro = membro.class;
 
-        Field[] atributos = ClasseMembro.getDeclaredFields();
+            Field[] atributos = ClasseMembro.getDeclaredFields();
 
-        System.out.println("Atributos da classe membro:");
+            System.out.println("Atributos da classe membro:");
 
-        for (Field field : atributos) {
-            String NomeAtributo = field.getName();
-            Class<?> TipoAtributo = field.getType();
-            System.out.println("Nome do atributo: " + NomeAtributo);
-            System.out.println("Tipo do atributo: " + TipoAtributo.getSimpleName());
+            for (Field field : atributos) {
+                String NomeAtributo = field.getName();
+                Class<?> TipoAtributo = field.getType();
+                System.out.println("Nome do atributo: " + NomeAtributo);
+                System.out.println("Tipo do atributo: " + TipoAtributo.getSimpleName());
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao imprimir atributos da classe membro: " + e.getMessage());
         }
     }
     
     public void ImprimeMetodosMembro() {
-        Class<?> ClasseMembro = membro.class;
+        try {
+            Class<?> ClasseMembro = membro.class;
 
-        Method[] metodos = ClasseMembro.getDeclaredMethods();
+            Method[] metodos = ClasseMembro.getDeclaredMethods();
 
-        System.out.println("Métodos da classe membro:");
+            System.out.println("Métodos da classe membro:");
 
-        for (Method method : metodos) {
-            String NomeMetodo = method.getName();
-            Class<?> TipoRetorno = method.getReturnType();
-            System.out.println("Nome do método: " + NomeMetodo);
-            System.out.println("Tipo de retorno: " + TipoRetorno.getSimpleName());
+            for (Method method : metodos) {
+                String NomeMetodo = method.getName();
+                Class<?> TipoRetorno = method.getReturnType();
+                System.out.println("Nome do método: " + NomeMetodo);
+                System.out.println("Tipo de retorno: " + TipoRetorno.getSimpleName());
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao imprimir métodos da classe membro: " + e.getMessage());
+        }
+    }
+    
+    public void ImprimeAtributoMultimidia() {
+        try {
+            Class<?> ClasseMultimidia = Multimidia.class;
+
+            Field[] atributos = ClasseMultimidia.getDeclaredFields();
+
+            System.out.println("Atributos da classe Multimidia:");
+
+            for (Field field : atributos) {
+                String NomeAtributo = field.getName();
+                Class<?> TipoAtributo = field.getType();
+                System.out.println("Nome do atributo: " + NomeAtributo);
+                System.out.println("Tipo do atributo: " + TipoAtributo.getSimpleName());
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao imprimir atributos da classe Multimidia: " + e.getMessage());
+        }
+    }
+    
+    public void ImprimeMetodosMultimidia() {
+        try {
+            Class<?> ClasseMultimidia = Multimidia.class;
+
+            Method[] metodos = ClasseMultimidia.getDeclaredMethods();
+
+            System.out.println("Métodos da classe Multimidia:");
+
+            for (Method method : metodos) {
+                String NomeMetodo = method.getName();
+                Class<?> TipoRetorno = method.getReturnType();
+                System.out.println("Nome do método: " + NomeMetodo);
+                System.out.println("Tipo de retorno: " + TipoRetorno.getSimpleName());
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao imprimir métodos da classe Multimidia: " + e.getMessage());
         }
     }
 }
